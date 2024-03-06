dce04f1221b7789ecfd7ab4d842fcb395caa9269
NAVEEN <naveenchowdaryvemuri@gmail.com>
2024-03-06 10:59:52 +0530

    DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        boolean d=true;
+        System.out.println(d);
+    }
+}
