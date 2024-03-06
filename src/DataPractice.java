e28a9e15cb1ba506993a6b8ad3cf5eec37094117
anusham428 <162035138+anusham428@users.noreply.github.com>
2024-03-06 11:22:41 +0530

    Create DataPractice.java
    added data types

---
+++ src/DataPractice.java
@@ -0,0 +1,8 @@
+public class DataPractice {
+    public static void main(String[] args) {
+        int a=3;
+        int b=6;
+        System.out.println(a);
+        System.out.println(a+b);
+    }
+}
