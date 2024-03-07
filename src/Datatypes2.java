d80824cf71e23b74c031852793e4458bd7c53ac4
prasannasamineni <162083638+prasannasamineni@users.noreply.github.com>
2024-03-07 16:33:03 +0530

    Update Datatypes2.java
    added multiplication

--- src/Datatypes2.java
+++ src/Datatypes2.java
@@ -3,7 +3,7 @@ public class Datatypes2
     public static void main(String[] args) {
         int a = 20;
         int b = 4;
-        int c = a + b;
+        int c = a * b;
         System.out.println(c);
     }
 
