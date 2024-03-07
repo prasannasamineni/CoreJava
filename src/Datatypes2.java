950a7e41cc5aaddbeed0d7cdcfffae20f857a839
prasannasamineni <162083638+prasannasamineni@users.noreply.github.com>
2024-03-07 16:13:58 +0530

    Create Datatypes2.java
    added division

---
+++ src/Datatypes2.java
@@ -0,0 +1,10 @@
+public class Datatypes2
+{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+    }
