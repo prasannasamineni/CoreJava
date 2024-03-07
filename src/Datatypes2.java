da200497d92a1e2b0e0ea470c85d9140dc25bdd2
prasannasamineni <162083638+prasannasamineni@users.noreply.github.com>
2024-03-07 18:28:13 +0530

    Update Datatypes2.java

--- src/Datatypes2.java
+++ src/Datatypes2.java
@@ -1,17 +1,19 @@
-public class Datatypes2
-{
+public class Datatypes2 {
     public static void main(String[] args) {
         int a = 20;
         int b = 4;
         int c = a * b;
-        int d= a+b;
-        int e=c+d;
-                System.out.println(c);
-            System.out.println(d);
-            System.out.println(e);
-            System.out.println("hi");
-            System.out.println("h r u");
+        int d = a + b;
+        int e = c + d;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("hi");
+        System.out.println("h r u");
+        {
+            for (int i = 0; i <= 5; i++)
 
+                System.out.println(i);
+        }
     }
-
-    }
+}
