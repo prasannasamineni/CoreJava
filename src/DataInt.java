195a34a41b3b9d80c7ada900b94bf7f77d37e21c
anusham428 <162035138+anusham428@users.noreply.github.com>
2024-03-07 10:11:37 +0530

    java src

---
+++ src/DataInt.java
@@ -0,0 +1,12 @@
+public class DataInt {
+    public static void main(String[] args) {
+        int a=10;
+        int b=7;
+        int c=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+
+}
