e50d5dc3388c263c2d032ea3b278e3f35ca4ecf5
NAVEEN <naveenchowdaryvemuri@gmail.com>
2024-03-07 10:58:38 +0530

    Variable Types

---
+++ src/VariableTypes.java
@@ -0,0 +1,13 @@
+public class VariableTypes {
+   static int c=10;
+    static int e=10;
+    public static void main(String[] args) {
+            int a=10;
+            int b=20;
+        System.out.println(a+b);
+        System.out.println(c);
+        System.out.println(e);
+    }
+    //int d=c+a  //here we can't able to use a variable because a is local variable in the main method
+
+}
